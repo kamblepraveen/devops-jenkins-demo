25568d2b6c219256267032a3bdbafc43327cf0e9
Praveen Kamble <kamblepraveen875107@gmail.com>
2026-09-13 12:31:16 +0000

    Add Linux Jenkins CI pipeline2

--- src/main/java/com/devops/demo/App.java
+++ src/main/java/com/devops/demo/App.java
@@ -1,9 +1,20 @@
 package com.devops.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("DevOps CI/CD Demo Application");
-        System.out.println("Application is running successfully!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "DevOps CI/CD Demo Application is running!";
     }
 }
