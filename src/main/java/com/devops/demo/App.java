48a852f9f9e45abcc171d947faf6c9e7b73f7e03
Praveen Kamble <kamblepraveen875107@gmail.com>
2026-09-13 06:53:11 +0000

    Initial DevOps demo application

---
+++ src/main/java/com/devops/demo/App.java
@@ -0,0 +1,9 @@
+package com.devops.demo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("DevOps CI/CD Demo Application");
+        System.out.println("Application is running successfully!");
+    }
+}
---
+++ src/test/java/com/devops/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.devops.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void applicationTest() {
+        assertTrue(true);
+    }
+}
