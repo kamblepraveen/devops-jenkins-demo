57927bd1e5837bbc9b2a9c75eb27782acc3b0935
Praveen Kamble <kamblepraveen875107@gmail.com>
2026-09-17 20:55:07 +0530

    Add Terraform instance variable

--- devops-jenkins-demo/terraform/ec2.tf
+++ devops-jenkins-demo/terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "devops_lab" {
   ami           = "ami-08188a5a4dfdbd573"
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "devops-lab-server"
